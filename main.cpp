636fa75e5f99a7533b1cbb74f6d7841609749645
TheJonu <the.jonu@gmail.com>
2021-03-12 21:29:47 +0100

    Some changes

--- main.cpp
+++ main.cpp
@@ -12,55 +12,108 @@ using namespace boost::filesystem;
 
 
 enum command{
-    bad_command, scan_dir_command, close_app_command
+    bad_command, scan_dir_command, scan_file_command, close_app_command
 };
 
+struct scan_result{
+    bool is_safe;
+};
 
 command accept_input_command();
-string accept_input_directory();
-void scan_directory_linear(path directory_path);
-bool scan_file(path file_path);
+string accept_input_file_path();
+string accept_input_directory_path();
+void scan_directory(path directory_path);
+scan_result scan_file(const path& file_path);
 string execute(const char* cmd);
 void close();
 
 
 command accept_input_command()
 {
-    cout << " 1 - Scan a directory" << endl;
-    cout << " 2 - Close the application" << endl;
+    cout << " 1 - Scan a file" << endl;
+    cout << " 2 - Scan a directory" << endl;
+    cout << " 3 - Close the application" << endl;
     cout << "Please choose a command: ";
     int c;
     cin >> c;
 
     switch(c){
-        case 1: return scan_dir_command;
-        case 2: return close_app_command;
+        case 1: return scan_file_command;
+        case 2: return scan_dir_command;
+        case 3: return close_app_command;
         default: return bad_command;
     }
 }
 
-string accept_input_directory()
+string accept_input_file_path()
 {
-    cout << "Please specify a directory: ";
-    string dir_path;
-    cin >> dir_path;
+    string file_path;
+    bool is_file;
 
-    if(!is_directory(dir_path)){
-        cout << "That's not a directory" << endl;
-        return "";
+    do{
+        cout << "Please specify a file path: ";
+        cin >> file_path;
+
+        if(!is_regular_file(file_path)){
+            cout << "That's not a file." << endl;
+        }
+        else{
+            is_file = true;
+        }
     }
+    while(!is_file);
+
+    return file_path;
+}
+
+string accept_input_directory_path()
+{
+    string dir_path;
+    bool is_dir;
+
+    do{
+        cout << "Please specify a directory path: ";
+        cin >> dir_path;
+
+        if(!is_directory(dir_path)){
+            cout << "That's not a directory." << endl;
+        }
+        else{
+            is_dir = true;
+        }
+    }
+    while(!is_dir);
 
     return dir_path;
 }
 
-void scan_directory_linear(path directory_path)
+void scan_directory(path directory_path)
 {
+    int file_count = 0;
+    for(recursive_directory_iterator iterator(directory_path); iterator != recursive_directory_iterator(); ++iterator)
+    {
+        if (is_regular_file(iterator -> path()))
+        {
+            file_count++;
+        }
+    }
+    cout << "Found " << file_count << " files at " << directory_path.string() << endl;
 
+    int number = 0;
+    for(recursive_directory_iterator iterator(directory_path); iterator != recursive_directory_iterator(); ++iterator)
+    {
+        if (is_regular_file(iterator -> path()))
+        {
+            cout << "--- " << ++number << "/" << file_count << " --------------------------------" << endl;
+            scan_file(iterator -> path());
+        }
+    }
 }
 
-bool scan_file(path file_path)
+scan_result scan_file(const path& file_path)
 {
     cout << "File: \t" << file_path.filename().string() << endl;
+    cout << "Path: \t" << file_path.parent_path().string() << endl;
 
     string hash = md5_of_file(file_path.string());
     cout << "Hash: \t" << hash << endl;
@@ -68,23 +121,22 @@ bool scan_file(path file_path)
     string requestStr = "whois -h hash.cymru.com " + hash;
     const char* request = &requestStr[0];
 
-    string result = execute(request);
-    int pos = result.find("NO_DATA");
-    cout << "Resp: \t" << result;
+    string response = execute(request);
+    int pos = response.find("NO_DATA");
+
+    scan_result result{};
 
     cout << "Safe: \t";
     if(pos != string::npos){
         cout << "yes" << endl;
-        return false;
+        result.is_safe = false;
     }
     else{
         cout << "NOT SAFE - MALWARE DETECTED" << endl;
-        return true;
+        result.is_safe = true;
     }
 
-    cout << result << endl;
-
-    return true;
+    return result;
 }
 
 string execute(const char* cmd)
@@ -105,49 +157,30 @@ void close()
 {
     cout << endl;
     cout << "That's all! Thanks for using me!" << endl;
-    cout << "Press anything to exit...";
-    getchar();
 }
 
 int main(int argc, char *argv[])
 {
-    // accept command input
-
-    //accept_input_command();
-
-    // accept directory input
-
-    string input_path;
-    do input_path = accept_input_directory();
-    while (input_path.empty());
-    path full_path = canonical(input_path);
-
-    // count files
-
-    int file_count = 0;
-    for(recursive_directory_iterator iterator(full_path); iterator != recursive_directory_iterator(); ++iterator)
-    {
-        if (is_regular_file(iterator -> path()))
+    command comm = accept_input_command();
+    switch(comm){
+        case scan_file_command:
         {
-            file_count++;
+            string file_path = accept_input_file_path();
+            scan_file(canonical(file_path));
+            close();
+            break;
+        }
+        case scan_dir_command:
+        {
+            string dir_path = accept_input_directory_path();
+            scan_directory(canonical(dir_path));
+            close();
+            break;
+        }
+        case close_app_command:
+        {
+            close();
+            break;
         }
     }
-    cout << "Found " << file_count << " files at " << full_path.string() << endl;
-
-    // scan files
-
-    cout << "Beginning scan..." << endl;
-    int number = 0;
-    for(recursive_directory_iterator iterator(full_path); iterator != recursive_directory_iterator(); ++iterator)
-    {
-        if (is_regular_file(iterator -> path()))
-        {
-            cout << "--- " << ++number << "/" << file_count << " ---" << endl;
-            scan_file(iterator -> path());
-        }
-    }
-
-    // close
-
-    close();
 }
