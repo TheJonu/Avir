fa01a68a25b70732d7ec6b5b87d1bee314f03202
TheJonu <the.jonu@gmail.com>
2021-04-29 14:47:34 +0200

    Reverted installer, removed built file

--- installer.sh
+++ installer.sh
@@ -8,4 +8,7 @@ echo "Source dir:   $SRC_DIR"
 echo "Build dir:    $BUILD_DIR"
 echo "Install dir:  $INSTALL_DIR"
 
-cp "$BUILD_DIR/avir" "$INSTALL_DIR"
+sudo -u"$USER" mkdir -p "$BUILD_DIR"
+
+cmake -S "$SRC_DIR" -B "$BUILD_DIR"
+cmake --build "$BUILD_DIR" --target install
